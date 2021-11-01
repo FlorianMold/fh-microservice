d8e896dda5ebd3e42dd1771c83b35a613cb06e5d
Florian Mold <florian.mold@agnet.at>
2021-11-01 15:12:37 +0100

    feat: add logging to http-endpoints

--- src/main/kotlin/at/fh/microservice/controllers/ArticleController.kt
+++ src/main/kotlin/at/fh/microservice/controllers/ArticleController.kt
@@ -2,6 +2,7 @@ package at.fh.microservice.controllers
 
 import at.fh.microservice.entities.Article
 import at.fh.microservice.services.ArticleService
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
@@ -10,30 +11,50 @@ import org.springframework.web.server.ResponseStatusException
 @RequestMapping("/api/v1/articles")
 class ArticleController(private val articleService: ArticleService) {
 
+    private val log = LoggerFactory.getLogger(javaClass)
+    private val baseUrl = "/api/v1/articles"
+
     @GetMapping("")
-    fun findAll() = articleService.getAllArticles()
+    fun findAll(): Iterable<Article> {
+        log.info("GET $baseUrl")
+
+        return articleService.getAllArticles()
+    }
 
     @GetMapping("/{id}")
-    fun findById(@PathVariable("id") articleId: Long): Article =
-        articleService.getArticleById(articleId) ?: throw ResponseStatusException(
+    fun findById(@PathVariable("id") articleId: Long): Article {
+        log.info("GET $baseUrl/$articleId")
+
+        return articleService.getArticleById(articleId) ?: throw ResponseStatusException(
             HttpStatus.NOT_FOUND,
             "This article does not exist"
         )
+    }
 
     @PostMapping("")
-    fun createEmployee(@RequestBody payload: Article): Article = articleService.createArticle(payload)
+    fun createEmployee(@RequestBody payload: Article): Article {
+        log.info("POST $baseUrl", payload)
+
+        return articleService.createArticle(payload)
+    }
 
     @PutMapping("/{id}")
-    fun updateEmployeeById(@PathVariable("id") articleId: Long, @RequestBody payload: Article): Article =
-        articleService.updateArticleById(articleId, payload) ?: throw ResponseStatusException(
+    fun updateEmployeeById(@PathVariable("id") articleId: Long, @RequestBody payload: Article): Article {
+        log.info("PUT $baseUrl/$articleId", payload)
+
+        return articleService.updateArticleById(articleId, payload) ?: throw ResponseStatusException(
             HttpStatus.NOT_FOUND,
             "This article does not exist"
         )
+    }
 
     @DeleteMapping("/{id}")
-    fun deleteEmployeesById(@PathVariable("id") articleId: Long) =
-        articleService.deleteArticleById(articleId) ?: throw ResponseStatusException(
+    fun deleteEmployeesById(@PathVariable("id") articleId: Long): Long {
+        log.info("DELETE $baseUrl/$articleId")
+
+        return articleService.deleteArticleById(articleId) ?: throw ResponseStatusException(
             HttpStatus.NOT_FOUND,
             "This article does not exist"
         )
+    }
 }
--- src/main/kotlin/at/fh/microservice/controllers/UserController.kt
+++ src/main/kotlin/at/fh/microservice/controllers/UserController.kt
@@ -3,6 +3,7 @@ package at.fh.microservice.controllers;
 
 import at.fh.microservice.entities.User
 import at.fh.microservice.services.UserService
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
@@ -11,34 +12,51 @@ import org.springframework.web.server.ResponseStatusException
 @RequestMapping("/api/v1/users")
 class UserController(private val userService: UserService) {
 
+    private val log = LoggerFactory.getLogger(javaClass)
+    private val baseUrl = "/api/v1/articles"
+
     @GetMapping("")
-    fun findAll() = userService.getAllUsers()
+    fun findAll(): Iterable<User> {
+        log.info("GET $baseUrl")
+
+        return userService.getAllUsers()
+    }
 
     @GetMapping("/{login}")
-    fun findOne(@PathVariable login: String) =
-        userService.getByLogin(login) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "This user does not exist")
+    fun findOne(@PathVariable login: String): User {
+        log.info("GET $baseUrl/$login")
 
-    @GetMapping("/{id}")
-    fun findById(@PathVariable("id") userId: Long): User =
-        userService.getUserById(userId) ?: throw ResponseStatusException(
+        return userService.getByLogin(login) ?: throw ResponseStatusException(
             HttpStatus.NOT_FOUND,
             "This user does not exist"
         )
+
+    }
 
     @PostMapping("")
-    fun createEmployee(@RequestBody payload: User): User = userService.createUser(payload)
+    fun createEmployee(@RequestBody payload: User): User {
+        log.info("POST $baseUrl", payload)
+
+        return userService.createUser(payload)
+    }
 
     @PutMapping("/{id}")
-    fun updateEmployeeById(@PathVariable("id") userId: Long, @RequestBody payload: User): User =
-        userService.updateUserById(userId, payload) ?: throw ResponseStatusException(
+    fun updateEmployeeById(@PathVariable("id") userId: Long, @RequestBody payload: User): User {
+        log.info("PUT $baseUrl/$userId", payload)
+
+        return userService.updateUserById(userId, payload) ?: throw ResponseStatusException(
             HttpStatus.NOT_FOUND,
             "This user does not exist"
         )
+    }
 
     @DeleteMapping("/{id}")
-    fun deleteEmployeesById(@PathVariable("id") userId: Long) =
-        userService.deleteUserById(userId) ?: throw ResponseStatusException(
+    fun deleteEmployeesById(@PathVariable("id") userId: Long): Long {
+        log.info("DELETE $baseUrl/$userId")
+
+        return userService.deleteUserById(userId) ?: throw ResponseStatusException(
             HttpStatus.NOT_FOUND,
             "This user does not exist"
         )
+    }
 }
